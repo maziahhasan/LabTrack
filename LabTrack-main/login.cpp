bb148b020ec22fd63f99d0037806e31a72108045
Anooshay <anooshayk277@gmail.com>
2025-11-22 17:24:48 +0500

    Revert "Added gui"
    This reverts commit eabc1c3047e842766892a2072fc9220bd60c61b9.

--- LabTrack-main/login.cpp
+++ LabTrack-main/login.cpp
@@ -1,32 +1,14 @@
 #include "login.h"
 #include "ui_login.h"
-#include "ta_mainwindow.h"
 
-Login::Login(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::LoginWindow)
+Login::Login(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
-
-    // Connect sign in button
-    connect(ui->btnSignIn, &QPushButton::clicked, this, &Login::on_btnSignIn_clicked);
 }
 
 Login::~Login()
 {
     delete ui;
-}
-
-void Login::on_btnSignIn_clicked()
-{
-    // Simple authentication - for demo, accept any email/password and assume TA role
-    QString email = ui->txtEmail->text();
-    QString password = ui->txtPassword->text();
-
-    if (!email.isEmpty() && !password.isEmpty()) {
-        // For demo, assume TA ID 1
-        TAMainWindow *taWindow = new TAMainWindow(1, this->parentWidget());
-        taWindow->show();
-        this->accept(); // Close login dialog
-    }
 }
--- LabTrack-main/startpage.cpp
+++ LabTrack-main/startpage.cpp
@@ -1,8 +1,5 @@
 #include "startpage.h"
 #include "ui_startpage.h"
-#include "ta_mainwindow.h"
-#include "hod_mainwindow.h"
-#include "instructor_mainwindow.h"
 
 StartPage::StartPage(QWidget *parent) :
     QMainWindow(parent),
@@ -14,7 +11,7 @@ StartPage::StartPage(QWidget *parent) :
     // Connect buttons to open Login window
     connect(ui->btnAdmin, &QPushButton::clicked, loginWindow, &QDialog::show);
     connect(ui->btnInstructor, &QPushButton::clicked, loginWindow, &QDialog::show);
-    connect(ui->btnTA, &QPushButton::clicked, this, &StartPage::on_btnTA_clicked);
+    connect(ui->btnTA, &QPushButton::clicked, loginWindow, &QDialog::show);
     connect(ui->btnHOD, &QPushButton::clicked, loginWindow, &QDialog::show);
 }
 
@@ -22,20 +19,4 @@ StartPage::~StartPage()
 {
     delete ui;
     delete loginWindow;
-}
-
-void StartPage::on_btnTA_clicked()
-{
-    // For demo, assume TA ID 1
-    TAMainWindow *taWindow = new TAMainWindow(1, this);
-    taWindow->show();
-    this->hide();
-}
-
-void StartPage::on_btnHOD_clicked()
-{
-    // For demo, open HOD MainWindow
-    HODMainWindow *hodWindow = new HODMainWindow(this);
-    hodWindow->show();
-    this->hide();
 }
